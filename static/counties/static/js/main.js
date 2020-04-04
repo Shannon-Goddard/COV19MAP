c5af8ae287d6dfb49e518c4cf5f0874f7998a906
Shannon Goddard <Uthingkithunk@aol.com>
2020-04-04 11:36:44 -0700

    getting the bear necessities

---
+++ static/counties/static/js/main.js
@@ -0,0 +1,45 @@
+$("path, polyline").hover(function(e) {
+  // make tooltip visible
+  $('#info-box').css('display','block');
+  // get date from selector element
+  const Date = document.querySelector('#myList').value;
+  // filter the `data` array for counties just in that date
+  const filtered = data.filter(d => d.date == Date);
+  // filter counties of that date to just the one county matching the id of 
+  // the path that is being hovered on 
+  const county = filtered.filter(d => d.id == $(this).attr('id'))[0];
+  // create the html string to populate the tooltip with 
+  // as long as the key isn't 'id' then continue building
+  let county_html = '';
+  Object.entries(county).forEach(([key, value]) => {
+    if (key != 'id') {
+      county_html += `${key}: ${value}<br>`;
+    }
+  })
+  // change value of tooltip to html we just made
+  $('#info-box').html(county_html);
+});
+$("path, polyline").mouseleave(function(e) {
+  $('#info-box').css('display','none');
+});
+$(document).mousemove(function(e) {
+  $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
+  $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
+}).mouseover();
+var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+if(ios) {
+  $('a').on('click touchend', function() {
+    var link = $(this).attr('href');
+    window.open(link,'_blank');
+    return false;
+  });
+}
+function getOption() {
+  const selectElement = document.querySelector('#myList');
+  output = selectElement.value;
+  document.querySelector('.output').textContent = output;
+}
+
+
+
+
