3d422293870fda18ba052ce13bb483a8a55ee2dc
Shannon Goddard <Uthingkithunk@aol.com>
2020-04-06 19:43:03 -0700

    error handling for undefined and null added to main.js

--- static/counties/static/js/main.js
+++ static/counties/static/js/main.js
@@ -3,19 +3,22 @@ $("path, polyline, polygon").hover(function(e) {
   $('#info-box').css('display','block');
   // get date from selector element
   const Date = document.querySelector('#myList').value;
+  console.log(Date);
   // filter the `data` array for counties just in that date
   const filtered = data.filter(d => d.date == Date);
+  console.log(filtered);
   // filter counties of that date to just the one county matching the id of 
   // the path that is being hovered on 
   const county = filtered.filter(d => d.id == $(this).attr('id'))[0];
+  console.log(county);
   // create the html string to populate the tooltip with 
   // as long as the key isn't 'id' then continue building
   let county_html = '';
   Object.entries(county).forEach(([key, value]) => {
-    if (key != 'id') {
+    if (key !== 'id' && key !== 'undefined' && key !== 'null') {
       county_html += `${key}: ${value}<br>`;
     }
-  })
+  });
   // change value of tooltip to html we just made
   $('#info-box').html(county_html);
 });
